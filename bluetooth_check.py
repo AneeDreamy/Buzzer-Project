e7205221fbe9be2c41840ee43b035cb93bca4133
AneeDreamy <167413122+AneeDreamy@users.noreply.github.com>
2024-07-27 16:49:38 +0800

    Update bluetooth_check.py
    Added comments to guide the user.

--- bluetooth_check.py
+++ bluetooth_check.py
@@ -1,7 +1,5 @@
 import bluetooth
 
-
-
 ##This is to check if the device is there.. comment out if you're not using (or put this in a separate file)
 print("Performing inquiry...")
 nearby_devices = bluetooth.discover_devices(lookup_names=True)
@@ -9,7 +7,6 @@ print(f"Found {len(nearby_devices)} devices.")
 for addr, name in nearby_devices:
     print(f"Address: {addr}, Name: {name}")
 
-
-
-
+## Example output:
 # Address: 00:24:01:01:04:7E, Name: TEAMG
+# Copy down this address to the GUI_final.py dictionary. (see code!)
